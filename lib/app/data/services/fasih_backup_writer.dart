bc416954728aea53abb487886ad3baa55df66ba6
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2026-05-23 03:37:36 +0700

    fix(excel): inject missing theme file to suppress Excel repair warning
    Syncfusion xlsio sets defaultThemeVersion in workbook.xml but omits
    xl/theme/theme1.xml, causing Excel to prompt a repair on open. Post-
    process the raw ZIP bytes via the archive package to inject a minimal
    OOXML theme and update Content_Types.xml and workbook.xml.rels.

--- lib/app/data/services/fasih_backup_writer.dart
+++ lib/app/data/services/fasih_backup_writer.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'dart:io';
 
-import 'package:flutter_archive/flutter_archive.dart';
+import 'package:archive/archive.dart';
+import 'package:flutter_archive/flutter_archive.dart' as flutter_archive;
 import 'package:injectable/injectable.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
@@ -35,6 +37,131 @@ class FasihBackupWriter {
     _buildMetaSheet(workbook, template, respondentMeta, envJson);
     return workbook;
   }
+
+  List<int> buildBytes({
+    required FasihTemplate template,
+    required List<FasihRecord> records,
+    required List<RespondentMeta> respondentMeta,
+    required String envJson,
+  }) {
+    final wb = buildWorkbook(
+      template: template,
+      records: records,
+      respondentMeta: respondentMeta,
+      envJson: envJson,
+    );
+    final raw = wb.saveAsStream();
+    wb.dispose();
+    return _patchXlsx(raw);
+  }
+
+  /// Post-processes the raw bytes from Syncfusion xlsio to inject a minimal
+  /// theme file. Without this, Excel reports a repair warning because
+  /// workbook.xml declares defaultThemeVersion but no theme file is present.
+  List<int> _patchXlsx(List<int> bytes) {
+    final archive = ZipDecoder().decodeBytes(bytes);
+    var hasTheme = archive.any((f) => f.name == 'xl/theme/theme1.xml');
+    if (hasTheme) return bytes;
+
+    final patched = Archive();
+    for (final file in archive) {
+      if (file.name == '[Content_Types].xml') {
+        final content = utf8.decode(file.content as List<int>);
+        final updated = content.replaceFirst(
+          '</Types>',
+          '<Override PartName="/xl/theme/theme1.xml"'
+              ' ContentType="application/vnd.openxmlformats-officedocument.theme+xml"/>'
+              '</Types>',
+        );
+        final data = utf8.encode(updated);
+        patched.addFile(ArchiveFile('[Content_Types].xml', data.length, data));
+      } else if (file.name == 'xl/_rels/workbook.xml.rels') {
+        final content = utf8.decode(file.content as List<int>);
+        final updated = content.replaceFirst(
+          '</Relationships>',
+          '<Relationship Id="rId_theme"'
+              ' Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme"'
+              ' Target="theme/theme1.xml"/>'
+              '</Relationships>',
+        );
+        final data = utf8.encode(updated);
+        patched.addFile(
+          ArchiveFile('xl/_rels/workbook.xml.rels', data.length, data),
+        );
+      } else {
+        patched.addFile(file);
+      }
+    }
+
+    final themeData = utf8.encode(_minimalThemeXml);
+    patched.addFile(
+      ArchiveFile('xl/theme/theme1.xml', themeData.length, themeData),
+    );
+
+    return ZipEncoder().encode(patched);
+  }
+
+  static const _minimalThemeXml =
+      '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'
+      '<a:theme xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" name="Office Theme">'
+      '<a:themeElements>'
+      '<a:clrScheme name="Office">'
+      '<a:dk1><a:sysClr val="windowText" lastClr="000000"/></a:dk1>'
+      '<a:lt1><a:sysClr val="window" lastClr="FFFFFF"/></a:lt1>'
+      '<a:dk2><a:srgbClr val="44546A"/></a:dk2>'
+      '<a:lt2><a:srgbClr val="E7E6E6"/></a:lt2>'
+      '<a:accent1><a:srgbClr val="4472C4"/></a:accent1>'
+      '<a:accent2><a:srgbClr val="ED7D31"/></a:accent2>'
+      '<a:accent3><a:srgbClr val="A9D18E"/></a:accent3>'
+      '<a:accent4><a:srgbClr val="FFC000"/></a:accent4>'
+      '<a:accent5><a:srgbClr val="5B9BD5"/></a:accent5>'
+      '<a:accent6><a:srgbClr val="70AD47"/></a:accent6>'
+      '<a:hlink><a:srgbClr val="0563C1"/></a:hlink>'
+      '<a:folHlink><a:srgbClr val="954F72"/></a:folHlink>'
+      '</a:clrScheme>'
+      '<a:fontScheme name="Office">'
+      '<a:majorFont><a:latin typeface="Calibri Light"/><a:ea typeface=""/><a:cs typeface=""/></a:majorFont>'
+      '<a:minorFont><a:latin typeface="Calibri"/><a:ea typeface=""/><a:cs typeface=""/></a:minorFont>'
+      '</a:fontScheme>'
+      '<a:fmtScheme name="Office">'
+      '<a:fillStyleLst>'
+      '<a:solidFill><a:schemeClr val="phClr"/></a:solidFill>'
+      '<a:gradFill rotWithShape="1"><a:gsLst>'
+      '<a:gs pos="0"><a:schemeClr val="phClr"><a:lumMod val="110000"/><a:satMod val="105000"/><a:tint val="67000"/></a:schemeClr></a:gs>'
+      '<a:gs pos="50000"><a:schemeClr val="phClr"><a:lumMod val="105000"/><a:satMod val="103000"/><a:tint val="73000"/></a:schemeClr></a:gs>'
+      '<a:gs pos="100000"><a:schemeClr val="phClr"><a:lumMod val="105000"/><a:satMod val="109000"/><a:tint val="81000"/></a:schemeClr></a:gs>'
+      '</a:gsLst><a:lin ang="5400000" scaled="0"/></a:gradFill>'
+      '<a:gradFill rotWithShape="1"><a:gsLst>'
+      '<a:gs pos="0"><a:schemeClr val="phClr"><a:satMod val="103000"/><a:lumMod val="102000"/><a:tint val="94000"/></a:schemeClr></a:gs>'
+      '<a:gs pos="100000"><a:schemeClr val="phClr"><a:lumMod val="99000"/><a:satMod val="120000"/><a:shade val="78000"/></a:schemeClr></a:gs>'
+      '</a:gsLst><a:lin ang="5400000" scaled="0"/></a:gradFill>'
+      '</a:fillStyleLst>'
+      '<a:lnStyleLst>'
+      '<a:ln w="6350" cap="flat" cmpd="sng" algn="ctr"><a:solidFill><a:schemeClr val="phClr"/></a:solidFill><a:prstDash val="solid"/><a:miter lim="800000"/></a:ln>'
+      '<a:ln w="12700" cap="flat" cmpd="sng" algn="ctr"><a:solidFill><a:schemeClr val="phClr"/></a:solidFill><a:prstDash val="solid"/><a:miter lim="800000"/></a:ln>'
+      '<a:ln w="19050" cap="flat" cmpd="sng" algn="ctr"><a:solidFill><a:schemeClr val="phClr"/></a:solidFill><a:prstDash val="solid"/><a:miter lim="800000"/></a:ln>'
+      '</a:lnStyleLst>'
+      '<a:effectStyleLst>'
+      '<a:effectStyle><a:effectLst/></a:effectStyle>'
+      '<a:effectStyle><a:effectLst/></a:effectStyle>'
+      '<a:effectStyle><a:effectLst>'
+      '<a:outerShdw blurRad="57150" dist="19050" dir="5400000" algn="ctr" rotWithShape="0">'
+      '<a:srgbClr val="000000"><a:alpha val="63000"/></a:srgbClr>'
+      '</a:outerShdw>'
+      '</a:effectLst></a:effectStyle>'
+      '</a:effectStyleLst>'
+      '<a:bgFillStyleLst>'
+      '<a:solidFill><a:schemeClr val="phClr"/></a:solidFill>'
+      '<a:solidFill><a:schemeClr val="phClr"><a:tint val="95000"/><a:satMod val="170000"/></a:schemeClr></a:solidFill>'
+      '<a:gradFill rotWithShape="1"><a:gsLst>'
+      '<a:gs pos="0"><a:schemeClr val="phClr"><a:tint val="93000"/><a:satMod val="150000"/><a:shade val="98000"/><a:lumMod val="102000"/></a:schemeClr></a:gs>'
+      '<a:gs pos="50000"><a:schemeClr val="phClr"><a:tint val="98000"/><a:lumMod val="102000"/><a:satMod val="130000"/></a:schemeClr></a:gs>'
+      '<a:gs pos="100000"><a:schemeClr val="phClr"><a:shade val="63000"/><a:satMod val="120000"/></a:schemeClr></a:gs>'
+      '</a:gsLst><a:lin ang="5400000" scaled="0"/></a:gradFill>'
+      '</a:bgFillStyleLst>'
+      '</a:fmtScheme>'
+      '</a:themeElements>'
+      '</a:theme>';
 
   Future<File> buildBackupZip({
     required xlsio.Workbook workbook,
@@ -72,7 +199,7 @@ class FasihBackupWriter {
       }
 
       final outputFile = await _resolveOutputFile(outputName);
-      await ZipFile.createFromDirectory(
+      await flutter_archive.ZipFile.createFromDirectory(
         sourceDir: tempDir,
         zipFile: outputFile,
         recurseSubDirs: true,
